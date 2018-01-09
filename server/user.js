5efb99414849e7f2256877499864e63d5f0a13f7
wangwq <wangwq@wangweiangdeMBP.lan>
2018-01-09 21:09:18 +0800

    user.js

--- server/user.js
+++ server/user.js
@@ -4,6 +4,8 @@ const utils =require('utility');//md5
 const Router =express.Router();
 const model = require('./model');
 const User = model.getModel('user');
+//设置不向前台返回密码和版本号
+const _filter = { 'pwd': 0, '__v':0};
 
 Router.get('/list',function(req, res){
     //User.remove({}, function(e,d){})
@@ -13,30 +15,47 @@ Router.get('/list',function(req, res){
 })
 Router.post('/login',function(req, res){
     const {user, pwd} = req.body;
-    User.findOne({user, pwd:amd5Pwd(pwd)}, {'pwd':0}, function(err,doc){
+    User.findOne({user, pwd:amd5Pwd(pwd)}, _filter, function(err,doc){
         if(!doc){
             return res.json({code:1, msg:'用户名或密码错误'})
         }
+        res.cookie('userid',doc._id)
         return res.json({code:0, data:doc})
     })
 })
 Router.post('/register', function(req,res){
-    console.log(req.body);
+
     const {user, pwd, type} = req.body;
     User.findOne({user},function(err, doc){
         if(doc){
             return res.json({code:1, msg:'用户名重复'})
         }
-        User.create({user, type, pwd:amd5Pwd(pwd)}, function(e, d){
+        const userModel = new User({user, type, pwd:amd5Pwd(pwd)})
+        userModel.save(function(e,d){
             if(e){
                 return res.json({code:1, msg:'后端出错了'})
             }
-            return res.json({code:0})
+            const {user, type, _id} = d;
+            res.cookie('userid', _id)
+            return res.json({code:0, data:{user, type, _id}})
         })
+
     })
 })
 Router.get('/info',function(req,res){
-    return res.json({code:1})
+    const { userid } = req.cookies;
+    if( !userid ){
+        return res.json({ code : 1})
+    }
+    User.findOne({_id:userid}, _filter, function(err, doc){
+        if(err){
+            return res.json({code:1, msg:'后端出错'})
+        }
+        if(doc){
+            return res.json({code:0, data:doc})
+        }
+    })
+    //return res.json({code:1})
 })
 //加盐
 function amd5Pwd (pwd) {

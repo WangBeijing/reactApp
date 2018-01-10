5bfc6991d2fcd2115ee493e6857448a07bccc998
Wwq <760068496@qq.com>
2018-01-10 11:19:57 +0800

    登录状态redux

--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -1,24 +1,24 @@
 import axios from 'axios'
 import { getRedirectPath } from '../util'
-const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+//const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+//const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 const ERROR_MSG = 'ERROR_MSG';
-const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 const LOAD_DATA = 'LOAD_DATA';
+const AUTH_SUCCESS = 'AUTH_SUCCESS';
 //用户初始状态
 const initState={
     redirectTo:'',
-    isAuth:false,
+    //isAuth:false,
     msg:'',
     user:'',
     type:'',
 }
 //reducer
 export function user(state=initState, action){
+    console.log(action)
     switch (action.type) {
-        case REGISTER_SUCCESS:
-            return {...state, msg:'', redirectTo:getRedirectPath(action.payload), isAuth:true, ...action.payload}
-        case LOGIN_SUCCESS:
-            return {...state, msg:'', redirectTo:getRedirectPath(action.payload), isAuth:true, ...action.payload}
+        case AUTH_SUCCESS:
+            return {...state, msg:'', redirectTo:getRedirectPath(action.payload),  ...action.payload}
         case LOAD_DATA:
             return {...state, ...action.payload}
         case ERROR_MSG:
@@ -27,12 +27,15 @@ export function user(state=initState, action){
             return state
     }
 }
-function registerSuccess(data){
-    return {type:REGISTER_SUCCESS, payload:data}
+function authSuccess(data){
+    return {type:AUTH_SUCCESS, payload:data}
 }
-function loginSuccess(data){
-    return { type:LOGIN_SUCCESS, payload:data}
-}
+// function registerSuccess(data){
+//     return {type:REGISTER_SUCCESS, payload:data}
+// }
+// function loginSuccess(data){
+//     return { type:LOGIN_SUCCESS, payload:data}
+// }
 function errorMsg(msg){
     return {msg, type:ERROR_MSG }
 }
@@ -40,7 +43,20 @@ function errorMsg(msg){
 export function loadData(userinfo){
     return { type:LOAD_DATA, payload:userinfo }
 }
+export function update(data){
+  //console.log(data)
+  return dispatch=>{
+    axios.post('/user/update',data)
+      .then(res=>{
 
+        if(res.status==200 && res.data.code === 0){
+            dispatch(authSuccess(res.data.data))
+        }else{
+            dispatch(errorMsg(res.data.msg))
+        }
+      })
+  }
+}
 
 export function login({user, pwd}){
     if(!user || !pwd){
@@ -50,8 +66,7 @@ export function login({user, pwd}){
         axios.post('/user/login',{user, pwd })
             .then(res=>{
                 if(res.status==200 && res.data.code === 0){
-                    //dispatch(registerSuccess({user, pwd, type }))
-                    dispatch(loginSuccess(res.data.data))
+                    dispatch(authSuccess(res.data.data))
                 }else{
                     dispatch(errorMsg(res.data.msg))
                 }
@@ -69,7 +84,7 @@ export function regisger({user, pwd, repeatpwd, type}){
         axios.post('/user/register',{user, pwd, type })
             .then(res=>{
                 if(res.status==200 && res.data.code === 0){
-                    dispatch(registerSuccess({user, pwd, type }))
+                    dispatch(authSuccess({user, pwd, type }))
                 }else{
                     dispatch(errorMsg(res.data.msg))
                 }

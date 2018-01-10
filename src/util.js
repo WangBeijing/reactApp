84525dfe8c2601439614ea858e79b7dd411e6936
Wwq <760068496@qq.com>
2018-01-10 11:17:49 +0800

    util

--- src/util.js
+++ src/util.js
@@ -3,9 +3,11 @@ export function getRedirectPath({type, avatar}){
     //根据用户信息 返回跳转地址
     //user.type /boss /genius
     //user.avatar /bossinfo /geniusinfo
+    console.log(avatar)
     let url =( type==='boss')? '/boss': '/genius';
     if( !avatar){
         url+='info'
     }
+    console.log(`url:${url}`)
     return url
 }
